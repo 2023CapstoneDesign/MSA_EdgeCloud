4200d72d2083ef3337b6e3714bc6211e8448f549
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2023-03-26 19:09:15 +0900

    change time measurement.

--- performance_test/performance_script/repeat50.js
+++ performance_test/performance_script/repeat50.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs");
 const FormData = require("form-data");
-const axios = require("axios")
+const axios = require("axios");
 
 if (process.argv.length != 4){
     throw Error("usage: node repeat50.js [SERVER IP ADDRESS:PORT] [IMG FILE PATH]");
@@ -11,11 +11,42 @@ if (process.argv.length != 4){
 var serverAddress = process.argv[2];
 var port = 8080;
 
-// while (finished!=50);;
+// (async () => {
+
+//     promis = []
+//     for (let i = 0; i < 5; i++){
+
+//         const data = new FormData();
+//         data.append("img", fs.createReadStream(process.argv[3]));
+
+//         const config = {
+//             method: 'post',
+//             url:`http://${serverAddress}/check/bird`,
+//             port,
+//             data: data
+//         }
+//         promis.push(new Promise(async(res, rej)=>{
+//             await axios(config)
+//             .then((res)=>{
+//                 res= res.data;
+//                 console.log(res);
+//             })
+//             res(0);
+//         }))
+//     }
+
+//     const startTime = Date.now();
+
+//     await Promise.all(promis);
+
+//     const endTime = Date.now();
+
+//     console.log(`${(endTime - startTime) / 1000.0} second(s)`);
+// }) ();
 
 (async () => {
 
-    promis = []
+    cons = []
     for (let i = 0; i < 5; i++){
 
         const data = new FormData();
@@ -27,25 +58,19 @@ var port = 8080;
             port,
             data: data
         }
-        promis.push(new Promise(async(res, rej)=>{
-
-            await axios(config)
-            .then((res)=>{
-                res= res.data;
-                console.log(res);
-            })
-            res(0);
-        }))
+        cons.push(config);
     }
 
     const startTime = Date.now();
 
-    await Promise.all(promis);
+    for (var i = 0; i < 5; i++){
+        let result = (await axios(cons[i])).data;
+
+        console.log(result);
+    }
 
     const endTime = Date.now();
 
-    console.log(`${(endTime - startTime) / 1000.0} second(s)`);
+    console.log(`${(endTime - startTime) / 1000.0} seconds`);
 }) ();
-
-
 
