7f2f90be39de4afc7633d0f1193e01aa5a43269c
min1925k <min1925k@gmail.com>
2023-06-01 13:57:33 +0900

    feat: metrics express CPU, mem usage

--- performance_test/metrics.js
+++ performance_test/metrics.js
@@ -1,4 +1,6 @@
 const { Registry, Counter, Gauge, Histogram, Summary, register } = require('prom-client');
+const os = require('os');
+
 
 const prometheus = () => {
   const registry = new Registry();
@@ -70,6 +72,47 @@ Prometheus.create({
   name: 'summary',
   help: 'random summary for test',
 });
+Prometheus.create({
+  type: 'gauge',
+  name: 'cpu_usage',
+  help: 'CPU usage in percentage',
+});
+
+Prometheus.create({
+  type: 'gauge',
+  name: 'memory_usage',
+  help: 'Memory usage in bytes',
+});
+
+// CPU 사용량 측정 함수
+function getCpuUsage() {
+  const cpus = os.cpus();
+  let totalIdle = 0;
+  let totalTick = 0;
+
+  for (const cpu of cpus) {
+    for (const type in cpu.times) {
+      totalTick += cpu.times[type];
+    }
+    totalIdle += cpu.times.idle;
+  }
+
+  return {
+    idle: totalIdle / cpus.length,
+    total: totalTick / cpus.length
+  };
+}
+
+// CPU 사용량 측정 및 추가
+const cpuUsage = getCpuUsage();
+Prometheus.add({ name: 'cpu_usage', data: cpuUsage });
+
+// 메모리 사용량 측정 및 추가
+const memoryUsage = process.memoryUsage().rss; // RSS 메모리 사용량 (바이트 단위)
+Prometheus.add({ name: 'memory_usage', data: memoryUsage });
+
+
+
 
 module.exports = {
   Prometheus,
