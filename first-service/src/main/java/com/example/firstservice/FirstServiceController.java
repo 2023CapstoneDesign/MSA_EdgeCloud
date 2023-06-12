79e551f48219d6c8a10b3978a98161513cd4fa98
LAPTOP-SEEN5R81\qudtj <qudtjr9849@gmail.com>
2023-06-12 13:44:46 +0900

    Create POST API

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -3,12 +3,11 @@ package com.example.firstservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 @Slf4j
 @RestController
@@ -37,6 +36,49 @@ public class FirstServiceController {
         log.info("Server port = {}", request.getServerPort());
         return String.format("This is a message from First Service on PORT %s",
                 env.getProperty("local.server.port"));
+    }
+
+    @PostMapping("/bird")
+    public String bird(@RequestBody MultipartFile imageFile) {
+        try {
+            // 임시 폴더에 이미지 저장
+            File tempFile = File.createTempFile("image", imageFile.getOriginalFilename());
+            try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(tempFile))) {
+                stream.write(imageFile.getBytes());
+            }
+
+            // Python 파일 경로
+            String pythonScriptPath = "../../../../../../../performance_test/predict_model.py";
+
+            // 파라미터 전달을 위해 명령어 준비
+            String[] cmd = new String[3];
+            cmd[0] = "python"; // Python 인터프리터
+            cmd[1] = pythonScriptPath; // Python 파일 경로
+            cmd[2] = tempFile.getAbsolutePath(); // 이미지 파일 경로
+
+            // 명령어 실행
+            Process process = Runtime.getRuntime().exec(cmd);
+
+            // 실행 결과 읽기
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            // 프로세스 종료 대기
+            int exitCode = process.waitFor();
+
+            // 임시 파일 삭제
+            tempFile.delete();
+
+            // 결과 반환
+            return "Python script executed with output:\n" + output.toString() + "\nExit code: " + exitCode;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "Error processing the image.";
+        }
     }
 }
 
